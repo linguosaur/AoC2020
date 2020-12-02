96d4c8bdbb2b030b1d17f70aacdbabe08f48167f
linguosaur <36645379+linguosaur@users.noreply.github.com>
2020-12-02 15:21:07 -0500

    Add files via upload
    Solutions, parts 1 and 2

---
+++ day2.py
@@ -0,0 +1,37 @@
+import re, sys
+
+def readInput(filename):
+	lines = []
+	with open(filename) as inputfile:
+		for line in inputfile:
+			lines.append(line.rstrip())
+
+	return lines
+
+def countValidPasswords1(passwordLines):
+	valid = 0
+	for passwordLine in passwordLines:
+		match = re.fullmatch(r'(\d+)-(\d+) ([a-z]): ([a-z]+)', passwordLine)
+		(minFreq, maxFreq, char, password) = match.groups()
+		freq = password.count(char)
+		if freq >= int(minFreq) and freq <= int(maxFreq):
+			valid += 1
+
+	return valid
+
+def countValidPasswords2(passwordLines):
+	valid = 0
+	for passwordLine in passwordLines:
+		match = re.fullmatch(r'(\d+)-(\d+) ([a-z]): ([a-z]+)', passwordLine)
+		(pos1, pos2, char, password) = match.groups()
+		if (password[int(pos1)-1] == char) != (password[int(pos2)-1] == char):
+			valid += 1
+
+	return valid
+
+
+passwordLines = readInput(sys.argv[1])
+valid1 = countValidPasswords1(passwordLines)
+print(valid1)
+valid2 = countValidPasswords2(passwordLines)
+print(valid2)
